bbaeb9fd40d2c70b93369086a0d9462d60d76bf7
clavesi <34937562+RealRavenn@users.noreply.github.com>
2020-08-22 08:34:23 -0500

    Add button functionality

--- js/index.js
+++ js/index.js
@@ -1,4 +1,7 @@
 var allRepos = [];
+console.log('hello');
+var btnRepos = document.getElementById("btnRepos");
+btnRepos.addEventListener("click", getRepos);
 
 async function getRepos() {
     const url = "https://api.github.com/users/clavesi/repos";
@@ -8,7 +11,5 @@ async function getRepos() {
     result.forEach(i => {
         allRepos.push([i.name, i.html_url, i.description, i.language, i.created_at])
     });
+    console.log(allRepos);
 }
-
-getRepos();
-console.log(allRepos);
